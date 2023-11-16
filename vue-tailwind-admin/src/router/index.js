6fe740e4337fc54fc1aaffe71972ac0729fe3169
Luis Avelino <luuis.avelino@gmail.com>
2023-11-15 22:45:47 -0300

    added route auth

--- vue-tailwind-admin/src/router/index.js
+++ vue-tailwind-admin/src/router/index.js
@@ -2,172 +2,213 @@ import Router from 'vue-router'
 
 import Dashboard from '@/components/Dashboard'
 
-const routes = [
-  { 
-    path: '/',
-    redirect: { 
-      name: 'DashboardHome' 
-    } 
-  },
-  {
-    path: '/users',
-    component: Dashboard,
-    children: [
-      {
-        path: '/',
-        redirect: {
-          name: 'DashboardUsers'
-        }
-      },
-      {
-        path: 'dashboard',
-        name: 'DashboardUsers',
-        component: null
-      }
-    ]
-  },
-  {
-    path: 'preferences',
-    component: Dashboard,
-    children: [
-      {
-        path: '/',
-        redirect: {
-          name: 'DashboardPreferences'
-        }
-      },
-      {
-        path: 'dashboard',
-        name: 'DashboardPreferences',
-        component: null
-      }
-    ]
-  },
-  { 
-    path: '/dashboard', 
-    component: Dashboard, 
-    children: [
-      { 
-        path: '/', 
-        redirect: { 
-          name: 'DashboardHome' 
-        }
-      },
-      { 
-        path: 'home', 
-        name: 'DashboardHome', 
-        component: () => import('@/views/DashboardHome') 
-      }
-    ]
-  },
-  {
-    path: '/systems',
-    component: Dashboard,
-    children: [
-      {
-        path: '/',
-        redirect: {
-          name: 'DashboardSystems'
-        }
-      },
-      {
-        path: 'dashboard',
-        name: 'DashboardSystems',
-        component: () => import('@/views/systems/DashboardSystems')
-      }
-    ]
-  },
-  {
-    path: '/controls',
-    component: Dashboard,
-    children: [
-      {
-        path: '/',
-        redirect: {
-          name: 'DashboardControls'
-        }
-      },
-      {
-        path: 'dashboard',
-        name: 'DashboardControls',
-        component: () => import('@/views/controls/DashboardControls')
-      }
-    ]
-  },
-  {
-    path: '/schemes',
-    component: Dashboard,
-    children: [
-      {
-        path: '/',
-        redirect: {
-          name: 'DashboardSchemes'
-        }
-      },
-      {
-        path: 'dashboard',
-        name: 'DashboardSchemes',
-        component: () => import('@/views/schemes/DashboardSchemes')
-      }
-    ]
-  
-  },
-  {
-    path: '/configurations',
-    component: Dashboard,
-    children: [
-      {
-        path: '/',
-        redirect: {
-          name: 'DashboardConfigurations'
-        }
-      },
-      {
-        path: 'dashboard',
-        name: 'DashboardConfigurations',
-        component: () => import('@/views/configurations/DashboardConfigurations')
-      }
-    ]
-  },
-  {
-    path: '/notifications',
-    component: Dashboard,
-    children: [
-      {
-        path: '/',
-        redirect: {
-          name: 'DashboardNotifications'
-        }
-      },
-      {
-        path: 'dashboard',
-        name: 'DashboardNotifications',
-        component: () => import('@/views/notifications/DashboardNotifications')
-      }
-    ]
-  },
-  {
-    path: '/schedules',
-    component: Dashboard,
-    children: [
-      {
-        path: '/',
-        redirect: {
-          name: 'DashboardSchedules'
-        }
-      },
-      {
-        path: 'dashboard',
-        name: 'DashboardSchedules',
-        component: () => import('@/views/schedules/DashboardSchedules')
-      }
-    ]
-  },
-]
-
 const router = new Router({
-  mode: 'history',
-  routes
-})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      redirect: {
+        name: "DashboardHome",
+      },
+    },
+    {
+      path: "/login",
+      component: () => import("@/views/auth/LoginPage"),
+    },
+    {
+      path: "/users",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardUsers",
+          },
+        },
+        {
+          path: "dashboard",
+          name: "DashboardUsers",
+          component: null,
+        },
+      ],
+    },
+    {
+      path: "preferences",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardPreferences",
+          },
+        },
+        {
+          path: "dashboard",
+          name: "DashboardPreferences",
+          component: null,
+        },
+      ],
+    },
+    {
+      path: "/dashboard",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardHome",
+          },
+        },
+        {
+          path: "home",
+          name: "DashboardHome",
+          component: () => import("@/views/home/DashboardHome"),
+          meta: {
+            requiresAuth: true,
+          },
+        },
+      ],
+    },
+    {
+      path: "/systems",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardSystems",
+          },
+        },
+        {
+          path: "dashboard",
+          name: "DashboardSystems",
+          component: () => import("@/views/systems/DashboardSystems"),
+          meta: {
+            requiresAuth: true,
+          },
+        },
+      ],
+    },
+    {
+      path: "/controls",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardControls",
+          },
+        },
+        {
+          path: "dashboard",
+          name: "DashboardControls",
+          component: () => import("@/views/controls/DashboardControls"),
+          meta: {
+            requiresAuth: true,
+          },
+        },
+      ],
+    },
+    {
+      path: "/schemes",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardSchemes",
+          },
+        },
+        {
+          path: "dashboard",
+          name: "DashboardSchemes",
+          component: () => import("@/views/schemes/DashboardSchemes"),
+          meta: {
+            requiresAuth: true,
+          },
+        },
+      ],
+    },
+    {
+      path: "/configurations",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardConfigurations",
+          },
+        },
+        {
+          path: "dashboard",
+          name: "DashboardConfigurations",
+          component: () =>
+            import("@/views/configurations/DashboardConfigurations"),
+          meta: {
+            requiresAuth: true,
+          },
+        },
+      ],
+    },
+    {
+      path: "/notifications",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardNotifications",
+          },
+        },
+        {
+          path: "dashboard",
+          name: "DashboardNotifications",
+          component: () =>
+            import("@/views/notifications/DashboardNotifications"),
+          meta: {
+            requiresAuth: true,
+          },
+        },
+      ],
+    },
+    {
+      path: "/schedules",
+      component: Dashboard,
+      children: [
+        {
+          path: "/",
+          redirect: {
+            name: "DashboardSchedules",
+          },
+        },
+        {
+          path: "dashboard",
+          name: "DashboardSchedules",
+          component: () => import("@/views/schedules/DashboardSchedules"),
+          meta: {
+            requiresAuth: true,
+          },
+        },
+      ],
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('to', to);
+  
+  if (to.meta.requiresAuth) {
+    const token = localStorage.getItem('token');
+    if (token) {
+      next();
+    } else {
+      // next('/login');
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
 
 export default router
---
+++ vue-tailwind-admin/src/views/auth/LoginPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
